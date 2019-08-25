a8cb565719f06fe66d6d21bce033a28af411751d
Marine Mekhakyan <marinemekhakyan@gmail.com>
2019-08-25 12:23:32 -0700

    created constructor for letters to be guessed

---
+++ letter.js
@@ -0,0 +1,19 @@
+function Letter(character) {
+    this.character = character,
+    this.guessed = false,
+    this.charReturn = function() {
+        if ( this.guessed === true ) {
+            return this.character;
+        }
+        else {
+            return "-";
+        }
+    }
+    this.verify = function (userGuess) {
+        if (userGuess === this.character) {
+            this.guessed === true;
+            return true;
+        }
+    }
+}
+module.exports = Letter;
